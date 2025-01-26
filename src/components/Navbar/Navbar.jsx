184a49dfba499ec48a779b17bca11052839b25e7
RajdeepSarkar2000 <rajdeepsarkar999@gmail.com>
2025-01-26 23:37:57 +0530

    update navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+/* eslint-disable quotes */
+import React, { useState } from "react";
+// import { HiMenuAlt4 } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 // import { images } from '../../constants';
-import './Navbar.scss';
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -14,25 +15,25 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div> */}
       <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
 
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {/* <HiMenuAlt4 onClick={() => setToggle(true)} /> */}
 
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
+            // eslint-disable-next-line quotes
+            transition={{ duration: 0.85, ease: "easeOut" }}
           >
-            <HiX onClick={() => setToggle(false)} />
+            {/* <HiX onClick={() => setToggle(false)} /> */}
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              {["home", "about", "work", "skills", "contact"].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
