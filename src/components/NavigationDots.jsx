e722aa29c2d60eb747294f7fce4d4f3ec043016d
RajdeepSarkar2000 <rajdeepsarkar999@gmail.com>
2025-01-26 23:37:44 +0530

    update navigation dots

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,18 +1,22 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/anchor-has-content */
 
-import React from 'react';
+import React from "react";
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
-      <a
-        href={`#${item}`}
-        key={item + index}
-        className="app__navigation-dot"
-        style={active === item ? { backgroundColor: '#12a523' } : {}}
-      />
-    ))}
+    {["home", "about", "work", "skills", "testimonial", "contact"].map(
+      (item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: " #003366" } : {}}
+        />
+      )
+    )}
   </div>
 );
 
