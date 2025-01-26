a38ba0bf352b28405d3948ada24abb762d859552
RajdeepSarkar2000 <rajdeepsarkar999@gmail.com>
2025-01-26 23:38:09 +0530

    update about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import './About.scss';
-import { urlFor, client } from '../../client';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./About.scss";
+import { urlFor, client } from "../../client";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -18,20 +20,29 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">A user interface is like a  <span>Joke</span> <br />If you have to explain it, its not that <span>Good</span></h2>
+      <h3 className="header-text">
+        A <span> full-stack developer</span> is like a chef – I juggle both the
+        <span> front-end</span> seasoning and <span> back-end</span> grilling,
+        making sure everything tastes just <span> right</span> without you ever
+        seeing the recipe!
+      </h3>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
             <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="skill-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
@@ -40,7 +51,7 @@ const About = () => {
 };
 
 export default AppWrap(
-  MotionWrap(About, 'app__about'),
-  'about',
-  'app__whitebg',
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
 );
